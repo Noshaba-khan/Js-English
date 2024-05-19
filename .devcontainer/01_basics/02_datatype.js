607d9866951bc3560eea0d019292b6ebebee1235
Noshaba Khan <156739546+Noshaba-khan@users.noreply.github.com>
2024-05-19 16:25:29 +0000

    variables

---
+++ .devcontainer/01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict";// treat all JS code as newer version
+
+//alret(3+3)// we are using modejs , not browser
+
+console.log(3+3)
+// code readability that should be high//
+console.log("Noshaba")
+
+let age = 20
+let  isloginn = false
+let state;
+
+// number==> 2 to power 53
+// bigint
+// string==> ""
+// boolen ==> standalone value
+// undefined ==>
+// symbol ==> unique
+// null==> standalone value
+
+
+
+
